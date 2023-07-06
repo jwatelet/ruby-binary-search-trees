a19901026d494dcd24e43fdba1f76c6f52dc278c
Johan Watelet <johanwatelet@gmail.com>
2023-07-06 14:58:24 +0200

    add comparable

--- lib/node.rb
+++ lib/node.rb
@@ -1,9 +1,15 @@
 class Node
+
+  include Comparable
   attr_accessor :data, :left, :right
 
   def initialize(value)
     @data = value
     @left = nil
     @right = nil
+  end
+
+  def <=>(other)
+    data <=> other.data
   end
 end
